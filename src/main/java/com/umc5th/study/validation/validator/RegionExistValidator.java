302e07ca218d2d65fb9fa3b24566f242f0e24731
eomgerm <fishbread00@gmail.com>
2023-11-28 21:05:12 +0900

    :sparkles: Feat: 지역 존재 여부 확인 어노테이션 추가

---
+++ src/main/java/com/umc5th/study/validation/validator/RegionExistValidator.java
@@ -0,0 +1,33 @@
+package com.umc5th.study.validation.validator;
+
+import com.umc5th.study.base.code.status.ErrorStatus;
+import com.umc5th.study.service.StoreQueryService;
+import com.umc5th.study.validation.annotation.ExistRegion;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RegionExistValidator implements ConstraintValidator<ExistRegion, String> {
+
+    private final StoreQueryService storeQueryService;
+
+    @Override
+    public void initialize(ExistRegion constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        boolean isValid = storeQueryService.isRegionExists(value);
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.REGION_NOT_FOUND.getMessage()).addConstraintViolation();
+        }
+
+        return isValid;
+    }
+}
