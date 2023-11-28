9c623c090bafef2d4320afca988ff1c15aa632dc
eomgerm <fishbread00@gmail.com>
2023-11-28 21:32:55 +0900

    :sparkles: Feat: 장소 없을 때 에러 추가

--- src/main/java/com/umc5th/study/base/code/status/ErrorStatus.java
+++ src/main/java/com/umc5th/study/base/code/status/ErrorStatus.java
@@ -14,7 +14,9 @@ public enum ErrorStatus implements BaseCode {
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "COMMON_401", "잘못된 요청"),
     TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP_001", "테스트 에러"),
 
-    REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE_001", "해당 지역이 존재하지 않습니다.");
+    REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE_001", "해당 지역이 존재하지 않습니다."),
+    STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE_002", "해당 가게가 존재하지 않습니다."),
+    ;
 
     private final HttpStatus httpStatus;
     private final String code;
