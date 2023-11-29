330cf47ed95c09a45a17abe4bc8d5bfc62647de4
eomgerm <fishbread00@gmail.com>
2023-11-29 21:17:31 +0900

    :sparkles: Feat: 미션 없음 에러 추가

--- src/main/java/com/umc5th/study/base/code/status/ErrorStatus.java
+++ src/main/java/com/umc5th/study/base/code/status/ErrorStatus.java
@@ -18,6 +18,8 @@ public enum ErrorStatus implements BaseCode {
     STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE_002", "해당 가게가 존재하지 않습니다."),
 
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_001", "존재하지 않는 사용자입니다."),
+
+    MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "MISSION_001", "존재하지 않는 미션입니다."),
     ;
 
     private final HttpStatus httpStatus;
